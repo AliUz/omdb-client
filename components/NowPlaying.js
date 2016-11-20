3271ced5f67a369e326104ac3a3a80e43047a1cc
AliUz <aliuz1@gmail.com>
2016-11-20 14:49:49 +0200

    Test commit

--- components/NowPlaying.js
+++ components/NowPlaying.js
@@ -1,7 +1,10 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { StyleSheet, NavigatorIOS} from 'react-native';
+import {
+    StyleSheet,
+    NavigatorIOS
+} from 'react-native';
 
 import MovieList from './MovieList';
 
